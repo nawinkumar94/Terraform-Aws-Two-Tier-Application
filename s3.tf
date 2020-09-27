c1706b3fc3b0c7ad1ba4230f6639af66ceece130
Naveenkumar <nawinkumar94@gmail.com>
2020-09-27 17:38:56 +0530

    correction commit

--- s3.tf
+++ s3.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "inventory-bucket" {
-  bucket = "terraform-state-nv-7777"
-  acl    = "private"
+#resource "aws_s3_bucket" "inventory-bucket" {
+#  bucket = "terraform-state-nv-7777"
+#  acl    = "private"
 
-  tags = {
-    Name = "terraform-state-nv-7777"
-  }
-}
+#  tags = {
+#    Name = "terraform-state-nv-7777"
+#  }
+#}
